290d6d377e6245dd853d8380a4a9b3aadd130d74
Nick Prühs <dev@npruehs.de>
2016-08-02 21:24:17 +0200

    CHANGED: Example - Moved existing PonyGame code to PonyGameExample.

---
+++ make-ponygame-example.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+
+ECHO Compiling pony-game...
+REM Link to Windows Kits and native libraries.
+REM http://tutorial.ponylang.org/appendices/compiler-args.html
+ponyc -p "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.10240.0\ucrt\x64" -p "Bin\PonyGameNative\x64\Debug" -o Bin\PonyGameExample Source\PonyGameExample
+
+ECHO Copying native libs...
+COPY "Bin\PonyGameNative\x64\Debug\PonyGameNative.dll" "Bin\PonyGameExample"
+
+ECHO Copying resources...
+COPY "Source\PonyGameExample\*.png" "Bin\PonyGameExample"
+
+PAUSE
